2d50f7a95435c3b253e33bb32668f77ec9fee215
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2024-10-10 15:39:53 -0500

    release: prepare version 1.0.0 for production
    ntend/vite.config.js

--- backend/gym_project/urls.py
+++ backend/gym_project/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.urls import path, include
 from gym_app.admin import admin_site
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin_site.urls),    
     path('api/', include('gym_app.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
