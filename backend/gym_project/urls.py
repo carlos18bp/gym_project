f69f77b95a2fc9a5e879e378e41a10a1d2cd88f2
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2024-10-10 22:49:07 -0500

    chore: prepare version 1.0.1 for production with set up completed

--- backend/gym_project/urls.py
+++ backend/gym_project/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 from gym_app.admin import admin_site
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -8,6 +8,15 @@ urlpatterns = [
     path('admin/', admin_site.urls),    
     path('api/', include('gym_app.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('sign_on/', TemplateView.as_view(template_name='index.html')),
+    path('forget_password/', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^process_list(?:/(?P<user_id>\d+))?(?:/(?P<display>\w+))?/$',
+        TemplateView.as_view(template_name='index.html')),
+    re_path(r'^process_detail/(?P<process_id>\d+)(?:/(?P<display>\w+))?/$', 
+        TemplateView.as_view(template_name='index.html')),
+    re_path(r'^process_form/(?P<action>\w+)(?:/(?P<process_id>\d+))?/$', 
+        TemplateView.as_view(template_name='index.html')),
+    path('directory_list/', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
