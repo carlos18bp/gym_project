2974b1e7bd3dd7e58afef3aa7aea1eb5ccfc6c3a
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2024-10-26 19:05:56 -0500

    feat: prepare version 1.1.0 - Add Policy Privacy view and Terms and conditions view

--- backend/static/frontend/notification_message.js
+++ backend/static/frontend/notification_message.js
@@ -1,1 +1,1 @@
-import{S as o}from"./sweetalert2.esm.all.js";const s="/static/frontend/signIn.jpg";function a(t,i){o.fire({title:t,icon:i})}export{s as _,a as s};
+import{S as i}from"./sweetalert2.esm.all.js";const s="/static/frontend/logo2.png",a="/static/frontend/signIn.jpg";function r(t,o){i.fire({title:t,icon:o})}export{s as _,a,r as s};
