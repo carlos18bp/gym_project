205b9231027b1061ba73efcb1ee1159d1417e17d
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2024-10-27 00:18:10 -0500

    fix: Bug in the paths for icons in vite configuration.

--- frontend/static/frontend/login_with_google.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./index.js";import{s}from"./notification_message.js";const l=async(o,e,t)=>{try{const r=await i.post("/api/google_login/",new URLSearchParams({token:o.credential}));t.login(r.data),s("¡Registro exitoso!","success"),e.push({name:"process_list"})}catch(r){a(r)}},a=o=>{console.error("Error during login:",o),s("Error durante el registro: ","error")};export{l};
--- frontend/static/frontend/notification_message.js
+++
@@ -1,1 +0,0 @@
-import{S as i}from"./sweetalert2.esm.all.js";const s="/static/frontend/logo2.png",a="/static/frontend/signIn.jpg";function r(t,o){i.fire({title:t,icon:o})}export{s as _,a,r as s};
