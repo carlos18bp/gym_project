a67b94f06efc0cb105e68f775abb2ab2b6cbfd56
Gustavo Adolfo Pérez Pérez <87864256+gustavop-dev@users.noreply.github.com>
2024-10-27 03:18:31 -0500

    chore: Add No Connection view for no connection time. (#15)

---
+++ frontend/src/views/offline/NoConnection.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-selected-background w-screen h-screen flex justify-center items-center">
+        <div class="flex flex-col items-center justify-center space-y-2 text-center">
+            <GlobeAmericasIcon class="w-40 h-40 text-primary"></GlobeAmericasIcon>
+            <h1 class="text-4xl text-primary font-semibold">¡Parece que no hay internet!</h1>
+            <p class="text-xl text-gray-500 font-regular">Revisa tu conexión para seguir navegando.</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {GlobeAmericasIcon} from '@heroicons/vue/24/outline';
+</script>
