cad0b4841b0cce938e73b6703175e8e51f7fb3a5
carlos18bp <carlos18bp@gmail.com>
2024-12-12 04:57:30 +0000

    fix: Add IsAuthenticated validation to create legal request endpoint.

--- backend/gym_app/views/legal_user_link.py
+++ backend/gym_app/views/legal_user_link.py
@@ -1,10 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from gym_app.models import LegalUserLink
 from gym_app.serializers import LegalUserLinkSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def list_legal_user_links(request, user_id):
     legal_user_links = LegalUserLink.objects.filter(user_id=user_id)
     serializer = LegalUserLinkSerializer(legal_user_links, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
