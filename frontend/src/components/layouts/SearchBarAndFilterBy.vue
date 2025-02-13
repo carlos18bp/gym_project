e6b17008df90a2f6a91e6bc202da194dc9cac1b5
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2025-02-13 10:47:12 -0500

    feat: correct PWA setting

--- frontend/src/components/layouts/SearchBarAndFilterBy.vue
+++ frontend/src/components/layouts/SearchBarAndFilterBy.vue
@@ -17,7 +17,7 @@
         <input
           id="search-field"
           class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-          placeholder="Search"
+          placeholder="Buscar"
           type="search"
           name="search"
           v-model="searchQuery"
