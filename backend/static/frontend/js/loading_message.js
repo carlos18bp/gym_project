71f9d73706e3cd68bfe806b75ee3f7150148e5d2
gustavop-dev <gustavop-dev@users.noreply.github.com>
2025-12-19 19:44:11 +0000

    feat: requirements for december 2025

--- backend/static/frontend/js/loading_message.js
+++ backend/static/frontend/js/loading_message.js
@@ -1,1 +1,1 @@
-import{al as o}from"./index.js";function i(s="Procesando...",a="Por favor espere, estamos procesando la solicitud."){o.fire({title:s,text:a,iconColor:"#FFF",allowOutsideClick:!1,showConfirmButton:!1,didOpen:()=>{o.showLoading()}})}function n(){o.close()}export{n as h,i as s};
+import{S as o}from"./index.js";function a(s="Procesando...",e="Por favor espere, estamos procesando la solicitud."){o.fire({title:s,text:e,iconColor:"#FFF",allowOutsideClick:!1,showConfirmButton:!1,didOpen:()=>{o.showLoading()}})}function n(){o.close()}export{n as h,a as s};
