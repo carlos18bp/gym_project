46cdf1615f49c6143d451632d1e7497a877f11a0
Gustavo Adolfo Pérrez Pérez <ryzepeck@srv614758.hstgr.cloud>
2026-04-29 16:21:04 +0000

    FEAT: split lawyer toolbar into two rows and guard editor render on currentUser

--- frontend/src/views/dynamic_document/DocumentEditor.vue
+++ frontend/src/views/dynamic_document/DocumentEditor.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="flex h-screen w-full">
     <Editor
+      v-if="userStore.currentUser"
+      :key="userStore.currentUser.id"
       :api-key="tinymceApiKey"
       cloud-channel="7"
       v-model="editorContent"
@@ -506,8 +508,11 @@ const editorConfig = computed(() => ({
   menubar: isLawyer.value ? "edit insert format table" : "",
   toolbar_mode: 'wrap',
   toolbar: isLawyer.value
-    ? (isClient.value ? "save return" : "save continue return") +
-      " | undo redo | formatselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | blocks fontsize lineheight | forecolor | removeformat | hr | table tableprops tablecellprops tablerowprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol | tablemergecells tablesplitcells"
+    ? [
+        (isClient.value ? "save return" : "save continue return") +
+          " | undo redo | formatselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | blocks fontsize lineheight | forecolor | removeformat | hr",
+        "table tableprops tablecellprops tablerowprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol | tablemergecells tablesplitcells",
+      ]
     : "save return | undo redo | formatselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | outdent indent | blocks fontsize lineheight | forecolor | removeformat | hr",
   contextmenu: isLawyer.value ? "table" : "",
   height: "100vh",
